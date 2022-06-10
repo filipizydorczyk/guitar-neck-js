a7194d0cadfb43b8ac4c4a94fb3f898aecbd8863
Filip Izydorczyk <filip.izydorczyk@protonmail.com>
2022-06-10 21:43:01 +0200

    dev: some tests

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,8 +1,32 @@
 import { GuitarNeck, NeckPosition } from "./types";
 
+/**
+ * Function to draw guitar nexk text representation
+ *
+ * @param neck set of informations that describes how neck
+ * will look like. Some of the informations are optional.
+ * Default values for them are
+ *  - `root` - it depends on tuning. `E` for `standard`,
+ * `D` for `drop` and `C` for `open`
+ *  - `strings` - `6`, needs to be non negative
+ *  - `freats` - `4`, needs to be non negative
+ * @param notes list of positions to draw on neck
+ * @returns string with neck representation
+ */
 export const guitarNeckToText = (
     neck: GuitarNeck,
     notes: NeckPosition[]
 ): string => {
     return "";
+};
+
+export const readFromText = (
+    text: string
+): { neck: GuitarNeck; notes: NeckPosition[] } | undefined => {
+    return {
+        neck: {
+            tuning: "open",
+        },
+        notes: [],
+    };
 };
--- tests/utils.test.ts
+++ tests/utils.test.ts
@@ -1,13 +1,246 @@
-describe("Utils tests", () => {
-    it("should create guitar neck in E standard tuning", () => {
-        console.log("Hello world");
-    });
-    it("should create guitar neck in D drop tuning", () => {});
-    it("should create guitar neck in open C tuning", () => {});
-    it("should create guitar neck in cis drop tuning", () => {});
-    it("should create guitar neck with default string number", () => {});
-    it("should create guitar neck with default freats number", () => {});
+import assert from "assert";
+import { GuitarNeck, guitarNeckToText, NeckPosition } from "../lib";
 
-    it("should not create guitar neck with negative or zero strings", () => {});
-    it("should not create guitar neck with negative or zero freats", () => {});
+describe("Utils tests", () => {
+    it("should create guitar neck in E standard", () => {
+        const neck: GuitarNeck = { tuning: "open" };
+        // am chord
+        const positions: NeckPosition[] = [
+            {
+                freat: 1,
+                string: 1,
+            },
+            {
+                freat: 2,
+                string: 2,
+            },
+            {
+                freat: 2,
+                string: 3,
+            },
+        ];
+        const response = guitarNeckToText(neck, positions);
+
+        const strings = response.split("\n");
+        assert.ok(strings.length === 6);
+        assert.ok(strings[0] === "E ||---|---|---|---|");
+        assert.ok(strings[1] === "B ||-o-|---|---|---|");
+        assert.ok(strings[2] === "G ||---|-o-|---|---|");
+        assert.ok(strings[3] === "D ||---|-o-|---|---|");
+        assert.ok(strings[4] === "A ||---|---|---|---|");
+        assert.ok(strings[5] === "E ||---|---|---|---|");
+    });
+    it("should create guitar neck in D drop tuning", () => {
+        const neck: GuitarNeck = { tuning: "drop" };
+        // f power chord
+        const positions: NeckPosition[] = [
+            {
+                freat: 3,
+                string: 5,
+            },
+            {
+                freat: 3,
+                string: 4,
+            },
+            {
+                freat: 3,
+                string: 3,
+            },
+        ];
+        const response = guitarNeckToText(neck, positions);
+
+        const strings = response.split("\n");
+        assert.ok(strings.length === 6);
+        assert.ok(strings[0] === "E ||---|---|---|---|");
+        assert.ok(strings[1] === "B ||---|---|---|---|");
+        assert.ok(strings[2] === "G ||---|---|---|---|");
+        assert.ok(strings[3] === "D ||---|---|-o-|---|");
+        assert.ok(strings[4] === "A ||---|---|-o-|---|");
+        assert.ok(strings[5] === "D ||---|---|-o-|---|");
+    });
+    it("should create guitar neck in open C tuning", () => {
+        const neck: GuitarNeck = { tuning: "open" };
+        // d chord
+        const positions: NeckPosition[] = [
+            {
+                freat: 2,
+                string: 0,
+            },
+            {
+                freat: 2,
+                string: 1,
+            },
+            {
+                freat: 2,
+                string: 2,
+            },
+            {
+                freat: 2,
+                string: 3,
+            },
+            {
+                freat: 2,
+                string: 4,
+            },
+            {
+                freat: 2,
+                string: 5,
+            },
+        ];
+        const response = guitarNeckToText(neck, positions);
+
+        const strings = response.split("\n");
+        assert.ok(strings.length === 6);
+        assert.ok(strings[0] === "C ||---|-o-|---|---|");
+        assert.ok(strings[1] === "G ||---|-o-|---|---|");
+        assert.ok(strings[2] === "C ||---|-o-|---|---|");
+        assert.ok(strings[3] === "G ||---|-o-|---|---|");
+        assert.ok(strings[4] === "C ||---|-o-|---|---|");
+        assert.ok(strings[5] === "E ||---|-o-|---|---|");
+    });
+    it("should create guitar neck in cis drop tuning", () => {
+        const neck: GuitarNeck = { tuning: "drop", root: "cis" };
+        // d power chord
+        const positions: NeckPosition[] = [
+            {
+                freat: 3,
+                string: 5,
+            },
+            {
+                freat: 3,
+                string: 4,
+            },
+            {
+                freat: 3,
+                string: 3,
+            },
+        ];
+        const response = guitarNeckToText(neck, positions);
+
+        const strings = response.split("\n");
+        assert.ok(strings.length === 6);
+        assert.ok(strings[0] === "dis ||---|---|---|---|");
+        assert.ok(strings[1] === "ais ||---|---|---|---|");
+        assert.ok(strings[2] === "fis ||---|---|---|---|");
+        assert.ok(strings[3] === "cis ||-o-|---|---|---|");
+        assert.ok(strings[4] === "gis ||-o-|---|---|---|");
+        assert.ok(strings[5] === "cis ||-o-|---|---|---|");
+    });
+    it("should create guitar neck with default string number", () => {
+        const neck: GuitarNeck = { tuning: "open" };
+        const positions: NeckPosition[] = [];
+        const response = guitarNeckToText(neck, positions);
+
+        const strings = response.split("\n");
+        assert.ok(strings.length === 6);
+    });
+    it("should create guitar neck with default freats number", () => {
+        const neck: GuitarNeck = { tuning: "open" };
+        const positions: NeckPosition[] = [];
+        const response = guitarNeckToText(neck, positions);
+
+        const strings = response.split("\n");
+        const stringElements = strings[0].split("|");
+        const freats = stringElements.slice(2, stringElements.length - 1);
+        assert.ok(freats.length === 4);
+    });
+    it("should create guitar neck with 7 strings", () => {
+        const neck: GuitarNeck = { tuning: "open", strings: 7 };
+        const positions: NeckPosition[] = [];
+        const response = guitarNeckToText(neck, positions);
+
+        const strings = response.split("\n");
+        assert.ok(strings.length === 7);
+    });
+    it("should create guitar neck with 12 freats", () => {
+        const neck: GuitarNeck = { tuning: "open", freats: 12 };
+        const positions: NeckPosition[] = [];
+        const response = guitarNeckToText(neck, positions);
+
+        const strings = response.split("\n");
+        const stringElements = strings[0].split("|");
+        const freats = stringElements.slice(2, stringElements.length - 1);
+        assert.ok(freats.length === 12);
+    });
+    it("should ignore posiotn when freat is 0", () => {
+        const neck: GuitarNeck = { tuning: "open" };
+        // am chord
+        const positions: NeckPosition[] = [
+            {
+                freat: 0,
+                string: 5,
+            },
+        ];
+        const response = guitarNeckToText(neck, positions);
+
+        const strings = response.split("\n");
+        assert.ok(strings.length === 6);
+        assert.ok(strings[0] === "E ||---|---|---|---|");
+        assert.ok(strings[1] === "B ||---|---|---|---|");
+        assert.ok(strings[2] === "G ||---|---|---|---|");
+        assert.ok(strings[3] === "D ||---|---|---|---|");
+        assert.ok(strings[4] === "A ||---|---|---|---|");
+        assert.ok(strings[5] === "E ||---|---|---|---|");
+    });
+
+    it("should not create guitar neck with negative strings", () => {
+        const neck: GuitarNeck = { tuning: "open", strings: -12 };
+        // am chord
+        const positions: NeckPosition[] = [
+            {
+                freat: 0,
+                string: 5,
+            },
+        ];
+        const response = guitarNeckToText(neck, positions);
+
+        assert.ok(response === "");
+    });
+    it("should not create guitar neck with negative freats", () => {
+        const neck: GuitarNeck = { tuning: "open", freats: -12 };
+        // am chord
+        const positions: NeckPosition[] = [
+            {
+                freat: 0,
+                string: 5,
+            },
+        ];
+        const response = guitarNeckToText(neck, positions);
+
+        assert.ok(response === "");
+    });
+    it("should not create guitar neck with zero strings", () => {
+        const neck: GuitarNeck = { tuning: "open", strings: 0 };
+        // am chord
+        const positions: NeckPosition[] = [
+            {
+                freat: 0,
+                string: 5,
+            },
+        ];
+        const response = guitarNeckToText(neck, positions);
+
+        assert.ok(response === "");
+    });
+    it("should not create guitar neck with zero freats", () => {
+        const neck: GuitarNeck = { tuning: "open", freats: 0 };
+        // am chord
+        const positions: NeckPosition[] = [
+            {
+                freat: 0,
+                string: 5,
+            },
+        ];
+        const response = guitarNeckToText(neck, positions);
+
+        assert.ok(response === "");
+    });
+
+    it("should omit positions out of neck range", () => {});
+
+    it("should fetch correct tuning from text", () => {});
+    it("should fetch number of freats tuning from text", () => {});
+    it("should fetch correct number of strings from text", () => {});
+    it("should fetch information from text with additional `-` cahracters", () => {});
+    it("should return undefined when text cant be properly fetched", () => {});
 });
