50f931e7da9ccb5c817b5fcb94676d8b1c45b4d6
Filip Izydorczyk <filip.izydorczyk@protonmail.com>
2022-06-12 01:26:41 +0200

    dev: tests done

--- tests/utils.test.ts
+++ tests/utils.test.ts
@@ -1,9 +1,14 @@
 import assert from "assert";
-import { GuitarNeck, guitarNeckToText, NeckPosition } from "../lib";
+import {
+    GuitarNeck,
+    guitarNeckToText,
+    NeckPosition,
+    readFromText,
+} from "../lib";
 
 describe("Utils tests", () => {
     it("should create guitar neck in E standard", () => {
-        const neck: GuitarNeck = { tuning: "open" };
+        const neck: GuitarNeck = { tuning: "standard" };
         // am chord
         const positions: NeckPosition[] = [
             {
@@ -235,12 +240,191 @@ describe("Utils tests", () => {
 
         assert.ok(response === "");
     });
+    it("should omit positions out of neck range", () => {
+        const neck: GuitarNeck = { tuning: "open" };
+        // am chord
+        const positions: NeckPosition[] = [
+            {
+                freat: 1,
+                string: 1,
+            },
+            {
+                freat: 2,
+                string: 2,
+            },
+            {
+                freat: 2,
+                string: 3,
+            },
+            {
+                freat: 2,
+                string: 7,
+            },
+            {
+                freat: 12,
+                string: 5,
+            },
+        ];
+        const response = guitarNeckToText(neck, positions);
 
-    it("should omit positions out of neck range", () => {});
+        const strings = response.split("\n");
+        assert.ok(strings.length === 6);
+        assert.ok(strings[0] === "E ||---|---|---|---|");
+        assert.ok(strings[1] === "B ||-o-|---|---|---|");
+        assert.ok(strings[2] === "G ||---|-o-|---|---|");
+        assert.ok(strings[3] === "D ||---|-o-|---|---|");
+        assert.ok(strings[4] === "A ||---|---|---|---|");
+        assert.ok(strings[5] === "E ||---|---|---|---|");
+    });
+    it("should fetch standard tuning from text", () => {
+        let neck = "";
+        neck += "E ||---|---|---|---|\n";
+        neck += "B ||-o-|---|---|---|\n";
+        neck += "G ||---|-o-|---|---|\n";
+        neck += "D ||---|-o-|---|---|\n";
+        neck += "A ||---|---|---|---|\n";
+        neck += "E ||---|---|---|---|\n";
 
-    it("should fetch correct tuning from text", () => {});
-    it("should fetch number of freats tuning from text", () => {});
-    it("should fetch correct number of strings from text", () => {});
-    it("should fetch information from text with additional `-` cahracters", () => {});
-    it("should return undefined when text cant be properly fetched", () => {});
+        const response = readFromText(neck);
+
+        if (!response) {
+            assert.ok(false);
+        }
+
+        assert.ok(response.neck.root === "e");
+        assert.ok(response.neck.tuning === "standard");
+    });
+    it("should fetch drop tuning from text", () => {
+        let neck = "";
+        neck += "E ||---|---|---|---|\n";
+        neck += "B ||---|---|---|---|\n";
+        neck += "G ||---|---|---|---|\n";
+        neck += "D ||---|---|-o-|---|\n";
+        neck += "A ||---|---|-o-|---|\n";
+        neck += "D ||---|---|-o-|---|\n";
+
+        const response = readFromText(neck);
+
+        if (!response) {
+            assert.ok(false);
+        }
+
+        assert.ok(response.neck.root === "d");
+        assert.ok(response.neck.tuning === "drop");
+    });
+    it("should fetch open tuning from text", () => {
+        let neck = "";
+        neck += "C ||---|-o-|---|---|\n";
+        neck += "G ||---|-o-|---|---|\n";
+        neck += "C ||---|-o-|---|---|\n";
+        neck += "G ||---|-o-|---|---|\n";
+        neck += "C ||---|-o-|---|---|\n";
+        neck += "E ||---|-o-|---|---|\n";
+
+        const response = readFromText(neck);
+
+        if (!response) {
+            assert.ok(false);
+        }
+
+        assert.ok(response.neck.root === "e");
+        assert.ok(response.neck.tuning === "standard");
+    });
+    it("should fetch number of freats from text", () => {
+        let neck = "";
+        neck += "E ||---|---|---|---|\n";
+        neck += "B ||-o-|---|---|---|\n";
+        neck += "G ||---|-o-|---|---|\n";
+        neck += "D ||---|-o-|---|---|\n";
+        neck += "A ||---|---|---|---|\n";
+        neck += "E ||---|---|---|---|\n";
+
+        const response = readFromText(neck);
+
+        if (!response) {
+            assert.ok(false);
+        }
+
+        assert.ok(response.neck.freats === 4);
+    });
+    it("should fetch correct number of strings from text", () => {
+        let neck = "";
+        neck += "E ||---|---|---|---|\n";
+        neck += "B ||-o-|---|---|---|\n";
+        neck += "G ||---|-o-|---|---|\n";
+        neck += "D ||---|-o-|---|---|\n";
+        neck += "A ||---|---|---|---|\n";
+        neck += "E ||---|---|---|---|\n";
+
+        const response = readFromText(neck);
+
+        if (!response) {
+            assert.ok(false);
+        }
+
+        assert.ok(response.neck.strings === 6);
+    });
+    it("should fetch standard tuning with correct notes number", () => {
+        let neck = "";
+        neck += "E ||---|---|---|---|\n";
+        neck += "B ||-o-|---|---|---|\n";
+        neck += "G ||---|-o-|---|---|\n";
+        neck += "D ||---|-o-|---|---|\n";
+        neck += "A ||---|---|---|---|\n";
+        neck += "E ||---|---|---|---|\n";
+
+        const response = readFromText(neck);
+
+        if (!response) {
+            assert.ok(false);
+        }
+
+        assert.ok(response.neck.root === "e");
+        assert.ok(response.neck.tuning === "standard");
+        assert.ok(response.notes.length === 3);
+    });
+    it("should fetch information from text with additional `-` cahracters", () => {
+        let neck = "";
+        neck += "E ||-----|----|---|-----|\n";
+        neck += "B ||--o--|----|---|-----|\n";
+        neck += "G ||-----|-o--|---|-----|\n";
+        neck += "D ||-----|--o-|---|-----|\n";
+        neck += "A ||-----|----|---|-----|\n";
+        neck += "E ||-----|----|---|-----|\n";
+
+        const response = readFromText(neck);
+
+        if (!response) {
+            assert.ok(false);
+        }
+
+        assert.ok(response.neck.root === "e");
+        assert.ok(response.neck.tuning === "standard");
+        assert.ok(response.notes.length === 3);
+    });
+    it("should fetch standard tuning when string is not ideal", () => {
+        const neck = `
+        E ||---|---|---|---|
+        B ||-o-|---|---|---|
+        G ||---|-o-|---|---|
+        D ||---|-o-|---|---| 
+        A ||---|---|---|---|
+        E ||---|---|---|---|
+        `;
+
+        const response = readFromText(neck);
+
+        if (!response) {
+            assert.ok(false);
+        }
+
+        assert.ok(response.neck.root === "e");
+        assert.ok(response.neck.tuning === "standard");
+        assert.ok(response.notes.length === 3);
+    });
+    it("should return undefined when text cant be properly fetched", () => {
+        const incorrectText = "klfsdas";
+        const response = readFromText(incorrectText);
+        assert.ok(response === undefined);
+    });
 });
