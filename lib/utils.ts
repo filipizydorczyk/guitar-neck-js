7d15e3ebe951deaf8e9cbf7cb0f636b044d3e136
Filip Izydorczyk <filip.izydorczyk@protonmail.com>
2022-06-12 23:06:16 +0200

    dev: creating tuning tests

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,18 @@
-import { GuitarNeck, NeckPosition } from "./types";
+import { GuitarNeck, NeckPosition, Note } from "./types";
+
+/**
+ * Function to create guitar tuning
+ * @param neck object that contain informations to create
+ * tuning.
+ *  -   `strings` is 6 by default
+ *  -   `root` by default is deteminated by `tuning`. Read
+ *      more about this at {@link guitarNeckToText}
+ * @returns array of notes ordered by guitar strings (eg.
+ * [E, B, G, D, A, E] for standard e)
+ */
+export const getGuitarTune = (neck: GuitarNeck): Note[] => {
+    return [];
+};
 
 /**
  * Function to draw guitar nexk text representation
--- tests/utils.test.ts
+++ tests/utils.test.ts
@@ -1,5 +1,6 @@
 import assert from "assert";
 import {
+    getGuitarTune,
     GuitarNeck,
     guitarNeckToText,
     NeckPosition,
@@ -426,5 +427,52 @@ describe("Utils tests", () => {
         const incorrectText = "klfsdas";
         const response = readFromText(incorrectText);
         assert.ok(response === undefined);
+    });
+
+    it("should 6 string E standard", () => {
+        const response = getGuitarTune({ tuning: "standard" });
+
+        assert.ok(response.length === 6);
+        assert.deepEqual(response, ["e", "b", "g", "d", "a", "e"]);
+    });
+
+    it("should 7 string B standard", () => {
+        const response = getGuitarTune({ tuning: "standard" });
+
+        assert.ok(response.length === 6);
+        assert.deepEqual(response, ["e", "b", "g", "d", "a", "e", "b"]);
+    });
+
+    it("should 4 string C open", () => {
+        const response = getGuitarTune({ tuning: "standard", strings: 4 });
+
+        assert.ok(response.length === 4);
+        assert.deepEqual(response, ["c", "g", "c", "g"]);
+    });
+
+    it("should 6 string D drop", () => {
+        const response = getGuitarTune({ tuning: "drop" });
+
+        assert.ok(response.length === 6);
+        assert.deepEqual(response, ["e", "b", "g", "d", "a", "d"]);
+    });
+
+    it("should 7 string cis drop", () => {
+        const response = getGuitarTune({
+            root: "cis",
+            tuning: "drop",
+            strings: 7,
+        });
+
+        assert.ok(response.length === 7);
+        assert.deepEqual(response, [
+            "gis",
+            "dis",
+            "b",
+            "fis",
+            "cis",
+            "gis",
+            "cis",
+        ]);
     });
 });
